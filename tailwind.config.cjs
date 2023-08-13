9d1fa7a69d7a61a36e19ae76d66bbf46057d7606
Alex Clayton <alexander.clayton.email@gmail.com>
2023-08-13 18:58:09 -0500

    added about section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-blue": "#8fb3ff",
+        "primary-purple": "#690f9a",
+        "secondary-blue": "#002761",
+        "secondary-purple": "#b854ee",
+
+        "light-blue": "#e5eeff",
+      },
+      gridTemplateColumns: {
+        // 15 column grid
+        15: "repeat(15, minmax(0, 1fr))",
+      },
+    },
+  },
+  plugins: [],
+};
